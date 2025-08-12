246511edd25721b142fd0147e31b92025f3f450d
web3uservj <vj17092002@gmail.com>
2025-08-12 17:19:08 +0530

    Module 2 date 12-08-25

---
+++ phase-2-Training/Project/Module-2/SourceCode/Controllers/BookTicketController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MiniProject2.Context;
+using MiniProject2.Models;
+
+namespace MiniProject2.Controllers
+{
+    public class BookTicketController : Controller
+    {
+        public AppDbContext context { get; set; }
+        public BookTicketController(AppDbContext context)
+        {
+            this.context= context;
+        }
+
+        public IActionResult Index()
+        {
+            string gender = TempData["Gender"] as string;
+            List<Audience> allAudience;
+
+            if (!string.IsNullOrEmpty(gender))
+            {
+                allAudience = context.audiences.Where(a => a.Gender == gender).ToList();
+            }
+            else
+            {
+                allAudience = context.audiences.ToList();
+            }
+
+            var totalAudience = allAudience.Count();
+            var averageAge = Math.Round(allAudience.Average(a => a.Age), 2);
+
+            ViewBag.TotalAudience = totalAudience;
+            ViewBag.AverageAge = averageAge;
+
+            return View(allAudience);
+        }
+
+        public IActionResult BookTicket()
+        {
+            return View();
+        }
+        public IActionResult Ticket(Audience audience)
+        {
+            if (ModelState.IsValid)
+            {
+                context.audiences.Add(audience);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return RedirectToAction("BookTicket");
+            }
+        }
+        public IActionResult Delete(string name)
+        {
+            var audience = context.audiences.FirstOrDefault(a => a.Name == name);
+            context.audiences.Remove(audience);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult FilterByGender(string gender)
+        {
+            var audiences = context.audiences.Where(a => a.Gender == gender).ToList();
+            TempData["Gender"] = gender; 
+            return RedirectToAction("Index");
+        }
+        public IActionResult Details(string name)
+        {
+            var audience=context.audiences.FirstOrDefault(a => a.Name==name);
+            return View(audience);
+        }
+    }
+}
---
+++ phase-2-Training/Project/Module-2/SourceCode/Models/Audience.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace MiniProject2.Models
+{
+    public class Audience
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(25, MinimumLength = 5, ErrorMessage = "Name must be between 5 and 25 characters.")]
+        public string Name { get; set; }
+
+        public string Gender { get; set; }
+
+        [Range(6, 90, ErrorMessage = "Age must be greater than 5.")]
+        public int Age { get; set; }
+
+        public DateOnly DateOfBirth { get; set; }
+    }
+}
